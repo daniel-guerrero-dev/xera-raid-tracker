bf8fb8a4185c6b9dc8323a6fa2a359e5f73cc1d1
daniel-guerrero-dev <danielguerrerodev6@gmail.com>
2026-07-27 23:56:09 -0500

    skeleton for data

---
+++ src/Table/Raids.tsx
@@ -0,0 +1,66 @@
+type Player = {
+  name: string;
+  subgroup: "1" | "2";
+  role: "BoonDPS" | "DPS" | "Healer";
+};
+
+type Wing = {
+  name: string;
+  encounter: {
+    name: string;
+    subroles: ["Willbender", "Soloheal Druid"];
+  };
+};
+
+export const players: Player[] = [
+  {
+    name: "Cy",
+    subgroup: "2",
+    role: "BoonDPS",
+  },
+  {
+    name: "Kopek",
+    subgroup: "2",
+    role: "BoonDPS",
+  },
+  {
+    name: "Mapoking",
+    subgroup: "2",
+    role: "DPS",
+  },
+  {
+    name: "Wisteriya",
+    subgroup: "2",
+    role: "DPS",
+  },
+  {
+    name: "Wawa",
+    subgroup: "2",
+    role: "DPS",
+  },
+  {
+    name: "Fennix",
+    subgroup: "1",
+    role: "Healer",
+  },
+  {
+    name: "Dabdab",
+    subgroup: "1",
+    role: "BoonDPS",
+  },
+  {
+    name: "Nicky",
+    subgroup: "1",
+    role: "DPS",
+  },
+  {
+    name: "Mesmerizing",
+    subgroup: "1",
+    role: "DPS",
+  },
+  {
+    name: "Godbane",
+    subgroup: "1",
+    role: "DPS",
+  },
+];
